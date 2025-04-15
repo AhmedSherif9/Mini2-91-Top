7e3ee693702ad798459b491cb1e4fc679c9d7afe
ayadi99 <elayadi@ayadi.dev>
2025-04-15 11:09:00 +0200

    added Customer
    added all Customer components except ratting need more info

--- src/main/java/com/example/demo/models/Customer.java
+++ src/main/java/com/example/demo/models/Customer.java
@@ -1,4 +1,69 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "trip_id")
+    Trip trip;
+
+    public Customer(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    public Customer(String name, String email, String phoneNumber, Trip trip) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.trip = trip;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
 }
