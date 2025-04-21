2e5ce6f432df5ce425b4809f22fc20f58e998651
YoussefSelit <youssef.selit@gmail.com>
2025-04-21 12:26:26 +0200

    Payments part

--- src/main/java/com/example/demo/repositories/PaymentRepository.java
+++ src/main/java/com/example/demo/repositories/PaymentRepository.java
@@ -2,8 +2,19 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    @Query("SELECT p FROM Payment p WHERE p.trip.id = :tripId")
+    public List<Payment> findPaymentsByTripId(@Param("tripId") long tripId);
+
+    @Query("SELECT p FROM Payment p WHERE p.amount > :threshold")
+    public List<Payment> findPaymentsAboveThreshold(@Param("threshold") double threshold);
+
 }
--- src/main/java/com/example/demo/services/PaymentService.java
+++ src/main/java/com/example/demo/services/PaymentService.java
@@ -1,7 +1,59 @@
 package com.example.demo.services;
 
+import com.example.demo.models.Payment;
+import com.example.demo.repositories.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaymentService {
+    private final PaymentRepository paymentRepository;
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+
+    public Payment addPayment(Payment payment) {return paymentRepository.save(payment);}
+
+    public List<Payment> getAllPayments() {return paymentRepository.findAll();}
+
+    public Payment getPaymentById(Long id) {
+        Optional<Payment> OptionalPayment = paymentRepository.findById(id);
+        if (OptionalPayment.isPresent()) {
+            return OptionalPayment.get();
+        } else {
+            throw new RuntimeException("Payment not found");
+        }
+
+    }
+
+    public Payment updatePayment(Long id, Payment payment) {
+        Optional<Payment> optionalPayment = paymentRepository.findById(id);
+        if (optionalPayment.isPresent()) {
+            Payment existingPayment = optionalPayment.get();
+            existingPayment.setAmount(payment.getAmount());
+            existingPayment.setPaymentMethod(payment.getPaymentMethod());
+            existingPayment.setPaymentStatus(payment.isPaymentStatus());
+            return paymentRepository.save(existingPayment);
+        } else {
+            throw new RuntimeException("Payment with id " + id + " not found");
+        }
+    }
+
+    public List<Payment> findPaymentsByTripId(Long tripId) {
+        return paymentRepository.findPaymentsByTripId(tripId);
+    }
+
+    public List<Payment> findByAmountThreshold(Double threshold) {
+        return paymentRepository.findPaymentsAboveThreshold(threshold);
+    }
+
+    public void deletePayment(Long id) {
+        paymentRepository.deleteById(id);
+    }
+
 }
